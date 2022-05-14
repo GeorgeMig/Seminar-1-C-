3254e8b1957ee1c74bb9fc3a7f631d22cec8a6c7
George_M <msgoha555@yandex.ru>
2022-05-14 13:55:05 +0300

    add ввод ver. 2.0

--- Task_0/Program.cs
+++ Task_0/Program.cs
@@ -2,7 +2,7 @@
 // принимает число и выдаёт его квадрат (число
 // умноженное на само себя)
 
-int num = 5;
+int num = int.Parse(Console.ReadLine ());
 int numResult = num * num;
 Console.WriteLine(numResult);
 // пример функции по нахождению степени double numResult = Math.Pow(num, 5);
