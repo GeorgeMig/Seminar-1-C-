68908d7edde386ee0f7b2604751e2401a0c36e76
George_M <msgoha555@yandex.ru>
2022-05-14 17:16:38 +0300

    Final ver. Homework_2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//ПРОГРАММА, ПОЗВОЛЯЮЩАЯ ОПРЕДЕЛИТЬ НАИБОЛЬШЕЕ ЧИСЛО ИЗ ТРЁХ ВВЕДЕННЫХ ЦЕЛЫХ ЧИСЕЛ ПОЛЬЗОВАТЕЛЕМ
+Console.Write("Введите любое целое число => ");
+int a = int.Parse(Console.ReadLine ());
+Console.Write("ещё одно => ");
+int b = int.Parse(Console.ReadLine ());
+Console.Write("и последнее число => ");
+int c = int.Parse(Console.ReadLine ());
+int max = 0;
+if (a > b)
+{
+    max = a;
+}
+else 
+{
+    max = b;
+}
+if (max > c)
+{
+    max = max;
+}
+else 
+{
+    max = c;
+
+}
+Console.Write (max);
+Console.Write (" " + "- наибольшее число");
