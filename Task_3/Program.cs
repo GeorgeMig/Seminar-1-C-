716a4b37c52aefe997932d7a6da02e89c4ae38a9
George_M <msgoha555@yandex.ru>
2022-05-14 16:20:36 +0300

    Task_3 final ver.

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,8 +1,10 @@
 ﻿//ПРОГРАММА, ПОЗВОЛЯЮЩАЯ ОПРЕДЕЛИТЬ ЧИСЛА, ЛЕЖАЩИЕ МЕЖДУ ОТРИЦАТЕЛЬНЫМ И ПОЛОЖИТЕЛЬНЫМ ЗНАЧЕНИЯМИ ВВЕДЕННОГО ЧИСЛА ПОЛЬЗОВАТЕЛЕМ
+Console.Write("Введите целое число => ");
 int b = int.Parse(Console.ReadLine ());
 int i = -b - 1;
 while (i < b)
 {
     i= i + 1;
-    Console.WriteLine (i);
+    Console.Write (i + " ");
 }
+Console.WriteLine (" " + "(ВЫПОЛНЕНО)");
