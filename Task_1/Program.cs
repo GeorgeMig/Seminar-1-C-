1b98f1dff2379e37072b2b67ce51f5bb52dcd250
George_M <msgoha555@yandex.ru>
2022-05-14 14:52:36 +0300

    completed Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программ, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя)
+
+//int num = int.Parse(Console.ReadLine ());
+//int numResult = num * num;
+///Console.WriteLine(numResult);
+// пример функции по нахождению степени double numResult = Math.Pow(num, 5);
+
+
+int a = 5;
+int b = 25;
+int numResult = b / a;
+Console.WriteLine (numResult);
+
+if (numResult == a)
+{
+    Console.WriteLine( "Является квадратом.");
+}
+else 
+{
+    Console.WriteLine( " Не является квадратом.");
+}
