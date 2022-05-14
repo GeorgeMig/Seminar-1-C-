8245cca45b48a8da9cd24ec4eb8b4d9b7e714a2f
George_M <msgoha555@yandex.ru>
2022-05-14 13:21:55 +0300

    Task_0 ver.full

---
+++ Task_0/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программ, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя)
+
+int num = 5;
+int numResult = num * num;
+Console.WriteLine(numResult);
+// пример функции по нахождению степени double numResult = Math.Pow(num, 5);
+
