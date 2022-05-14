063bbd86f82388ef506f7b064cc419e891d09b8b
George_M <msgoha555@yandex.ru>
2022-05-14 15:21:31 +0300

    final Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿//ПРОГРАММА УКАЗЫВАЕТ КАКОМУ ЧИСЛУ СООТВЕТСТВУЕТ ДЕНЬ НЕДЕЛИ
+string numWeekDay = Console.ReadLine ();
+if (numWeekDay == "1") Console.WriteLine ("Monday");
+if (numWeekDay == "2") Console.WriteLine ("Tuesday");
+if (numWeekDay == "3") Console.WriteLine ("Wednesday");
+if (numWeekDay == "4") Console.WriteLine ("Thursday");
+if (numWeekDay == "5") Console.WriteLine ("Friday");
+if (numWeekDay == "6") Console.WriteLine ("Saturday");
+if (numWeekDay == "7") Console.WriteLine ("Sunday");
