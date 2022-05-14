ce408395f3bbaffb829dcd3cf2f4b79f5d36f9e7
George_M <msgoha555@yandex.ru>
2022-05-14 16:59:03 +0300

    Final ver. Homework_1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//ПРОГРАММА, ПОЗВОЛЯЮЩАЯ ОПРЕДЕЛИТЬ НАИБОЛЬШЕЕ ЧИСЛО ИЗ ДВУХ ВВЕДЕННЫХ ЦЕЛЫХ ЧИСЕЛ ПОЛЬЗОВАТЕЛЕМ
+Console.Write("Введите любое целое число => ");
+int a = int.Parse(Console.ReadLine ());
+Console.Write("и ещё одно => ");
+int b = int.Parse(Console.ReadLine ());
+int max = 0;
+if (a > b)
+{
+    max = a;
+    Console.Write (a);
+    Console.Write (" " + "- наибольшее число");
+}
+else 
+{
+    max = b;
+    Console.Write(b);
+    Console.Write (" " + "- наибольшее число");
+}
